6915fcce92e981abc06168d77d8f07353788397f
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2016-07-31 10:34:17 -0300

    Corrigido inversão do nome de cometa/grupo - removido a importação do pacote math

--- ovnis_lua_johnidm/main.lua
+++ ovnis_lua_johnidm/main.lua
@@ -1,5 +1,3 @@
-require "math"
-
 function retorna_numero_por_letra(letra)
 	local letras = {A=1, B=2, C=3, D=4, E=5, F=6, G=7, H=8, I=9, J=10, K=11, L=12, M=13, N=14, O=15, P=16, Q=17, R=18, S=19, T=20, U=21, V=22, W=23, X=24, Y=25, Z=26}
     return letras[letra]
@@ -22,8 +20,8 @@ function retornar_resto_45(numero)
     return math.fmod(numero, 45)
 end
 
-local grupo = {"HALLEY", "ENCKE", "WOLF", "KUSHIDA"}
-local cometa = {"AMARELO", "VERMELHO", "PRETO",  "AZUL"}
+local cometa = {"HALLEY", "ENCKE", "WOLF", "KUSHIDA"}
+local grupo = {"AMARELO", "VERMELHO", "PRETO",  "AZUL"}
 
 for i=1,4 do
 
